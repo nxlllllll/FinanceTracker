6e2d8f9950a8d2d142c1369acda4c1b7b068c164
nxlllllll <nxlllllll@mail.ru>
2026-07-24 16:56:12 +0300

    [+] added revoked-session check to JWT validation, rejecting tokens whose session was revoked or that lack a session id entirely

--- FinanceTracker.Infrastructure/Cache/SessionRevocationCacheKeys.cs
+++ FinanceTracker.Infrastructure/Cache/SessionRevocationCacheKeys.cs
@@ -1,6 +1,6 @@
 ﻿namespace FinanceTracker.Infrastructure.Cache;
 
-internal static class SessionRevocationCacheKeys
+public static class SessionRevocationCacheKeys
 {
 	public static string RevokedSessionKey(Guid sessionId)
 		=> $"revoked-session:{sessionId}";
