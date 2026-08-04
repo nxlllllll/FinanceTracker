deb4d71f95724c7c56d826e496b8e3be1c631fa7
nxlllllll <nxlllllll@mail.ru>
2026-08-04 18:15:05 +0300

    [-] removed an exact duplicate index on rm_transactions and four covered by composite ones

---
+++ FinanceTracker.Migrator/Migrations/V045__drop_redundant_indexes.sql
@@ -0,0 +1,5 @@
+drop index if exists idx_rm_transactions_account_id;
+drop index if exists idx_rm_transactions_user;
+drop index if exists idx_rm_transactions_user_date;
+drop index if exists idx_accounts_user;
+drop index if exists idx_budgets_user;
