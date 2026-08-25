9aadea771dbdbbdeaa3d73db3e1f00c0b2aab58d
nxlllllll <nxlllllll@mail.ru>
2026-08-25 21:03:12 +0300

    [+] added CancelTransactionCommand and its handler, returning the money and taking the analytics back [+] added CancellationOptions, defaulting to a thirty-day window measured from when the record was entered [+] added the cancellation branch to the transaction loader and to the audit log [+] added flow tests for the feed pair, the category total, the budget progress and the expired window [=] changed cancellation of an excluded transaction to leave the analytics untouched

---
+++ FinanceTracker.Application/UseCases/Transaction/Commands/CancelTransaction/CancelTransactionCommand.cs
@@ -0,0 +1,16 @@
+﻿using FinanceTracker.Application.Behaviours.Authorization;
+using FinanceTracker.Application.Behaviours.Idempotency;
+using FinanceTracker.Application.Behaviours.RateLimit;
+using FinanceTracker.Core.Exceptions;
+using FinanceTracker.Core.Results;
+using MediatR;
+
+namespace FinanceTracker.Application.UseCases.Transaction.Commands.CancelTransaction;
+
+public sealed record CancelTransactionCommand(
+	Guid UserId,
+	Guid TransactionId
+) : IIdempotentCommand, IRequest<Result<Guid, AppException>>, IAuthorizable, IUserScopedRequest
+{
+	public Guid IdempotencyKey { get; init; }
+}
---
+++ FinanceTracker.Application/UseCases/Transaction/Commands/CancelTransaction/CancelTransactionCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace FinanceTracker.Application.UseCases.Transaction.Commands.CancelTransaction;
+
+public sealed class CancelTransactionCommandValidator : AbstractValidator<CancelTransactionCommand>
+{
+	public CancelTransactionCommandValidator()
+	{
+		RuleFor(expression: x => x.UserId).NotEmpty();
+		RuleFor(expression: x => x.TransactionId).NotEmpty();
+	}
+}
